62d6063aefe0c23ebfea57f2022e9830aaa41442
adam <adamjleung123@gmail.com>
2021-12-24 18:49:45 +1100

    Timeline mobile compatibility

--- src/components/TimelinePanel.tsx
+++ src/components/TimelinePanel.tsx
@@ -1,5 +1,5 @@
 import { css } from "@emotion/react";
-import { Colours, Sizes } from "../util/constants";
+import { breakpoints, Colours, Sizes } from "../util/constants";
 
 interface Props {
   title: string;
@@ -12,44 +12,55 @@ interface Props {
 const TimeLinePanel = ({ title, subtitle, logo, logoPos, children }: Props) => {
   return (
     <div css={styles.panel}>
-      {logoPos === "left" && (
-        <div css={styles.logowrapper(logoPos)}>
-          <img src={logo} alt="logo" css={styles.logo} width={200}></img>
-        </div>
-      )}
+      <div css={styles.logowrapper(logoPos)}>
+        <img src={logo} alt="logo" css={styles.logo} width={200}></img>
+      </div>
 
       <div css={styles.information(logoPos)}>
         <div css={styles.title}>{title}</div>
         <div css={styles.subtitle}>{subtitle}</div>
         <div css={styles.children}>{children}</div>
       </div>
-
-      {logoPos === "right" && (
-        <div css={styles.logowrapper(logoPos)}>
-          <img src={logo} alt="logo" css={styles.logo} width={200}></img>
-        </div>
-      )}
     </div>
   );
 };
 
 const styles = {
-  panel: css({
-    display: "grid",
-    gridTemplateColumns: "1fr 1fr",
-  }),
+  panel: css(
+    breakpoints({
+      display: ["flex", "flex", "grid", "grid"],
+      flexDirection: ["column", "column", "initial", "initial"],
+      justifyContent: ["center", "center", "initial", "initial"],
+      alignItems: ["center", "center", "initial", "initial"],
+      padding: "0 1rem",
+      gridTemplateColumns: "[left] 1fr [right] 1fr",
+      gridTemplateRows: "1fr",
+      gridAutoFlow: "column",
+    })
+  ),
   logowrapper: (logoPos: string) =>
-    css({
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: logoPos === "left" ? "flex-end" : "flex-start",
-      paddingRight: logoPos === "left" ? "2em" : 0,
-      paddingLeft: logoPos === "right" ? "2em" : 0,
-      borderLeft: logoPos === "right" ? `1px dashed ${Colours.gray4}` : "",
-      paddingTop: "1rem",
-      paddingBottom: "1rem",
-    }),
+    css(
+      breakpoints({
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        gridColumn: logoPos,
+        alignItems: logoPos === "left" ? "flex-end" : "flex-start",
+        padding:
+          logoPos === "left"
+            ? ["1rem 0", "1rem 0", "1rem 2rem 1rem 0", "1rem 2rem 1rem 0"]
+            : ["1rem 0", "1rem 0", "1rem 0 1rem 2rem", "1rem 0 1rem 2rem"],
+        borderLeft:
+          logoPos === "right"
+            ? [
+                "none",
+                "none",
+                `1px dashed ${Colours.gray4}`,
+                `1px dashed ${Colours.gray4}`,
+              ]
+            : "",
+      })
+    ),
   logo: css({
     height: "200px",
     borderRadius: "50%",
@@ -57,18 +68,35 @@ const styles = {
     backgroundColor: Colours.white,
   }),
   information: (logoPos: string) =>
-    css({
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: logoPos === "left" ? "flex-start" : "flex-end",
-      paddingLeft: logoPos === "left" ? "2em" : 0,
-      paddingRight: logoPos === "right" ? "2em" : 0,
-      borderLeft: logoPos === "left" ? `1px dashed ${Colours.gray4}` : "",
-      paddingTop: "1rem",
-      paddingBottom: "1rem",
-      textAlign: logoPos === "left" ? "left" : "right",
-    }),
+    css(
+      breakpoints({
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems:
+          logoPos === "left"
+            ? ["center", "center", "flex-start", "flex-start"]
+            : ["center", "center", "flex-end", "flex-end"],
+
+        padding:
+          logoPos === "left"
+            ? ["1rem 0", "1rem 0", "1rem 0 1rem 2rem", "1rem 0 1rem 2rem"]
+            : ["1rem 0", "1rem 0", "1rem 2rem 1rem 0", "1rem 2rem 1rem 0"],
+        borderLeft:
+          logoPos === "left"
+            ? [
+                "none",
+                "none",
+                `1px dashed ${Colours.gray4}`,
+                `1px dashed ${Colours.gray4}`,
+              ]
+            : "",
+        textAlign:
+          logoPos === "left"
+            ? ["center", "center", "left", "left"]
+            : ["center", "center", "right", "right"],
+      })
+    ),
   title: css({
     fontSize: Sizes.xl,
     fontWeight: 700,
@@ -77,11 +105,11 @@ const styles = {
   subtitle: css({
     fontSize: Sizes.large,
     color: Colours.gray4,
-    paddingBottom: "1em",
+    paddingBottom: "0.5em",
   }),
   children: css({
     fontSize: Sizes.med,
-    maxWidth: "35vw",
+    maxWidth: "calc(min(100%, 700px))",
   }),
 };
 
